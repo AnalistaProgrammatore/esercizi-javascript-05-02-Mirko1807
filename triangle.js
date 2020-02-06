fa0e636b4937602066eba23118c040da801d7584
Mirko1807 <59826817+Mirko1807@users.noreply.github.com>
2020-02-06 10:46:58 +0100

    Update triangle.js

--- triangle.js
+++ triangle.js
@@ -1,3 +1,19 @@
 /**
 * Write a loop that makes seven calls to console.log to output the following triangle
 */
+
+let lenght = 10;
+let base = "#";
+let space = " "
+let spacel = lenght
+for (let i = 0; i < lenght; i++) {
+  for (let j = 0; j < spacel; j++) {
+    space += " ";
+  }
+  spacel--;
+
+  console.log(space + base);
+  base += "##"
+  space = " ";
+}
+
